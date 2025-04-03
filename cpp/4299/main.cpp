12732669e3d685a1f41e529d26cb0c2e37666ba1
MiniGalaxy <iiyeonghun5@gmail.com>
2025-04-04 01:44:06 +0900

    regular update

---
+++ cpp/4299/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int a, s;
+
+int main()
+{
+    cin >> a >> s;
+
+    if((a + s) % 2 == 1 || s > a) cout << -1;
+    else cout << (a + s) / 2 << ' ' << (a - s) / 2;
+
+    return 0;
+}
