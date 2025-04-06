a90442519f6bd4e00928ce17c7cbd308f6c0e01c
MiniGalaxy <iiyeonghun5@gmail.com>
2025-04-06 17:16:35 +0900

    regular update

--- cpp/2696/main.cpp
+++ cpp/2696/main.cpp
@@ -7,11 +7,15 @@ int T;
 
 int M;
 
-priority_queue<long long> l;
-priority_queue<long long, vector<long long>, greater<long long>> r;
+priority_queue<int> l;
+priority_queue<int, vector<int>, greater<int>> r;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     cin >> T;
 
     for(int i = 0; i < T; i++){
@@ -20,17 +24,42 @@ int main()
 
         cin >> M;
 
-        long long c;
+        int c;
         cin >> c;
 
-        cout << c;
+        cout << (M - 1) / 2 + 1 << '\n';
 
-        for(int j = 1; j < M; j++){
-            int t;
-            cin >> t;
+        cout << c << ' ';
 
+        for(int j = 0, cnt = 1; j < (M - 1) / 2; j++){
+            for(int k = 0, t; k < 2; k++){
+                cin >> t;
 
+                if(t > c) r.push(t);
+                else l.push(t);
+            }
+
+            while(l.size() > r.size()){
+                r.push(c);
+                c = l.top();
+                l.pop();
+            }
+            while(l.size() < r.size()){
+                l.push(c);
+                c = r.top();
+                r.pop();
+            }
+
+            if(cnt == 10){
+                cout << '\n';
+                cnt = 0;
+            }
+
+            cout << c << ' ';
+            cnt++;
         }
+
+        cout << '\n';
     }
 
     return 0;
